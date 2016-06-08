ec6939c656c22b16d0516e93b25584031b4a04f7
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 22:16:30 -0700

    init

---
+++ OutKeyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutKeyword
+{
+    class Program
+    {
+        private static void Write(out int x)
+        {
+            x = 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+
+        static int Main(string[] args)
+        {
+            int y = 3;
+            Write(out y);
+            return 0;
+        }
+    }
+}
